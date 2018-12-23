39451deecdacd03b02c7a80c51f330be87b2d947
CobaltXII <sidatt64@gmail.com>
2018-12-23 14:32:26 -0500

    Added manual Managed_Sprite* allocator

--- Source/Sprite.hpp
+++ Source/Sprite.hpp
@@ -28,3 +28,32 @@ struct Managed_Sprite
 	unsigned int Offset;
 };
 
+// Allocate a chunk of memory (on the CPU) that can hold Vertex_Count 2-dimensional vertices with
+// 2-dimensional texture coordinates.
+
+Managed_Sprite* Allocate_Sprite(unsigned int Vertex_Count)
+{
+	float* Memory = (float*)malloc(Vertex_Count * 4 * sizeof(float));
+
+	if (!Memory)
+	{
+		std::cout << "Could not allocate a sprite buffer for " << Vertex_Count << " vertices." << std::endl;
+
+		exit(16);
+	}
+
+	Managed_Sprite* The_Managed_Sprite = new Managed_Sprite();
+
+	The_Managed_Sprite->Vertices = Memory;
+
+	The_Managed_Sprite->Size_In_Bytes = Vertex_Count * 4 * sizeof(float);
+
+	The_Managed_Sprite->Size_In_Floats = Vertex_Count * 4;
+
+	The_Managed_Sprite->Size_In_Vertices = Vertex_Count;
+
+	The_Managed_Sprite->Offset = 0;
+
+	return The_Managed_Sprite;
+}
+
