65b089b86f92092ed7affee84a7c0c4c4cb38cce
CobaltXII <sidatt64@gmail.com>
2018-12-23 14:23:33 -0500

    Added list of all Block texture paths

---
+++ Source/Image.hpp
@@ -0,0 +1,96 @@
+// A list for every type of texture in the game.
+
+#include <string>
+#include <vector>
+
+// All blocks.
+
+std::vector<std::string> All_Block_Texture_Paths =
+{
+	"Blocks/bedrock.png",
+
+	"Blocks/birch_leaves.png",
+
+	"Blocks/birch_log_side.png",
+
+	"Blocks/birch_log_top.png",
+
+	"Blocks/birch_planks.png",
+
+	"Blocks/bookshelf.png",
+
+	"Blocks/brick.png",
+
+	"Blocks/coal_ore.png",
+
+	"Blocks/cobblestone.png",
+
+	"Blocks/diamond_ore.png",
+
+	"Blocks/dirt.png",
+
+	"Blocks/emerald_ore.png",
+
+	"Blocks/gold_ore.png",
+
+	"Blocks/grass_side.png",
+
+	"Blocks/grass_top.png",
+
+	"Blocks/gravel.png",
+
+	"Blocks/iron_ore.png",
+
+	"Blocks/mushroom_block_brown.png",
+
+	"Blocks/mushroom_block_red.png",
+
+	"Blocks/mushroom_block_stem_side.png",
+
+	"Blocks/mushroom_block_stem_top.png",
+
+	"Blocks/oak_leaves.png",
+
+	"Blocks/oak_log_side.png",
+
+	"Blocks/oak_log_top.png",
+
+	"Blocks/oak_planks.png",
+
+	"Blocks/sand.png",
+
+	"Blocks/stone.png",
+
+	"Blocks/wool_colored_black.png",
+
+	"Blocks/wool_colored_blue.png",
+
+	"Blocks/wool_colored_brown.png",
+
+	"Blocks/wool_colored_cyan.png",
+
+	"Blocks/wool_colored_gray.png",
+
+	"Blocks/wool_colored_green.png",
+
+	"Blocks/wool_colored_light_blue.png",
+
+	"Blocks/wool_colored_lime.png",
+
+	"Blocks/wool_colored_magenta.png",
+
+	"Blocks/wool_colored_orange.png",
+
+	"Blocks/wool_colored_pink.png",
+
+	"Blocks/wool_colored_purple.png",
+
+	"Blocks/wool_colored_red.png",
+
+	"Blocks/wool_colored_silver.png",
+
+	"Blocks/wool_colored_white.png",
+
+	"Blocks/wool_colored_yellow.png"
+};
+
