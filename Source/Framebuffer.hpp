a535978b13d9bf460cb9ea2e7d7d8121ad11ebdf
CobaltXII <sidatt64@gmail.com>
2018-12-23 14:21:55 -0500

    Added Frame_Buffer structure

---
+++ Source/Framebuffer.hpp
@@ -0,0 +1,12 @@
+// You can't use this for general purpose stuff! It only works for what I use it for.
+
+struct Frame_Buffer
+{
+	GLuint The_Frame_Buffer;
+
+	GLuint The_Render_Buffer;
+
+	GLuint The_Quad;
+
+	GLuint The_Texture;
+};
