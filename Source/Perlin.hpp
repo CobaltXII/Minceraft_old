da239a76abc68544d9cbd445789bdd8396b6707e
CobaltXII <sidatt64@gmail.com>
2018-12-23 14:28:06 -0500

    Added the Grad function

--- Source/Perlin.hpp
+++ Source/Perlin.hpp
@@ -23,3 +23,61 @@ struct Perlin
 		return a + t * (b - a);
 	}
 
+	// Gradient function.
+
+	static inline double Grad(int hash, double x, double y, double z)
+	{
+		int h = hash & 15;
+
+		double u;
+		double v;
+
+		if (h < 8)
+		{
+			u = x;
+		}
+		else
+		{
+			u = y;
+		}
+
+		if (h < 4)
+		{
+			v = y;
+		}
+		else
+		{
+			if (h == 12 || h == 14)
+			{
+				v = x;
+			}
+			else
+			{
+				v = z;
+			}
+		}
+
+		double a;
+		double b;
+
+		if ((h & 1) == 0)
+		{
+			a = u;
+		}
+		else
+		{
+			a = -u;
+		}
+
+		if ((h & 2) == 0)
+		{
+			b = v;
+		}
+		else
+		{
+			b = -v;
+		}
+
+		return a + b;
+	}
+
