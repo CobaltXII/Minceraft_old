9f73f1c28a64acb29f6dfd34c9c9c5da6172083d
CobaltXII <sidatt64@gmail.com>
2018-12-23 14:35:10 -0500

    Added Voxel typedef

---
+++ Source/World.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+// A world can be represented using a World type. A block can be represented using a Voxel type.
+// The world typename Voxel is used to avoid confusion with Block_ID.
+
+typedef unsigned short Voxel;
