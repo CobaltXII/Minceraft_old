27681105ab4a4ebd815df99d0c21e555538b813a
CobaltXII <sidatt64@gmail.com>
2018-12-23 14:24:40 -0500

    Added empty mat4 generator

--- Source/Matrix.hpp
+++ Source/Matrix.hpp
@@ -2,3 +2,16 @@
 
 typedef float* mat4;
 
+// Generate an empty mat4.
+
+mat4 Mat4_Empty()
+{
+	mat4 Out = (mat4)malloc(16 * sizeof(float));
+
+	for (int i = 0; i < 16; i++)
+	{
+		Out[i] = 0.0f;
+	}
+
+	return Out;
+}
