7ea265af70177beb3c082a13302150a0ba71594e
CobaltXII <sidatt64@gmail.com>
2018-12-23 14:17:11 -0500

    Added Block_ID enumeration

---
+++ Source/Block.hpp
@@ -0,0 +1,91 @@
+#include <vector>
+#include <string>
+#include <ostream>
+
+// All implemented blocks.
+
+enum Block_ID
+{
+	id_air,
+
+	id_bedrock,
+
+	id_stone,
+
+	id_cobblestone,
+
+	id_dirt,
+
+	id_grass,
+
+	id_sand,
+
+	id_gravel,
+
+	id_brick,
+
+	id_bookshelf,
+
+	id_oak_log,
+
+	id_oak_planks,
+
+	id_oak_leaves,
+
+	id_birch_log,
+
+	id_birch_planks,
+
+	id_birch_leaves,
+
+	id_red_mushroom_block,
+
+	id_brown_mushroom_block,
+
+	id_mushroom_stem,
+
+	id_coal_ore,
+
+	id_iron_ore,
+
+	id_gold_ore,
+
+	id_diamond_ore,
+
+	id_emerald_ore,
+
+	id_white_wool,
+
+	id_orange_wool,
+
+	id_magenta_wool,
+
+	id_light_blue_wool,
+
+	id_yellow_wool,
+
+	id_lime_wool,
+
+	id_pink_wool,
+
+	id_gray_wool,
+
+	id_light_gray_wool,
+
+	id_cyan_wool,
+
+	id_purple_wool,
+
+	id_blue_wool,
+
+	id_brown_wool,
+
+	id_green_wool,
+
+	id_red_wool,
+
+	id_black_wool,
+
+	id_last
+};
+
