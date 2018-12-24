a295f62ab3c55865f4a367eb24eaaa60964c759a
CobaltXII <sidatt64@gmail.com>
2018-12-23 21:44:49 -0500

    Chunks now have a seperate VBO and VAO for water

--- Source/Chunk.hpp
+++ Source/Chunk.hpp
@@ -17,7 +17,13 @@ struct Chunk
 
 	GLuint Buffer_Object;
 
+	GLuint Water_Vertex_Array;
+
+	GLuint Water_Buffer_Object;
+
 	unsigned int Vertex_Array_Size_In_Floats;
+
+	unsigned int Water_Vertex_Array_Size_In_Floats;
 
 	// The boolean value of the variable Live indicates whether this chunk is loaded onto the GPU
 	// or not. Chunks that are not loaded onto the GPU are not rendered.
@@ -32,6 +38,8 @@ struct Chunk
 	Chunk
 	(
 		unsigned int _Vertex_Array_Size_In_Floats,
+
+		unsigned int _Water_Vertex_Array_Size_In_Floats,
 
 		unsigned int _X, 
 		unsigned int _Y, 
@@ -56,7 +64,15 @@ struct Chunk
 		
 		Vertex_Array = 0;
 
+		Buffer_Object = 0;
+
+		Water_Vertex_Array = 0;
+
+		Water_Buffer_Object = 0;
+
 		Vertex_Array_Size_In_Floats = _Vertex_Array_Size_In_Floats;
+
+		Water_Vertex_Array_Size_In_Floats = _Water_Vertex_Array_Size_In_Floats;
 	}
 
 	// Destroy this chunk.
@@ -69,7 +85,13 @@ struct Chunk
 
 		glDeleteBuffers(1, &Buffer_Object);
 
+		glDeleteVertexArrays(1, &Water_Vertex_Array);
+
+		glDeleteBuffers(1, &Water_Buffer_Object);
+
 		Vertex_Array_Size_In_Floats = 0;
+
+		Water_Vertex_Array_Size_In_Floats = 0;
 	}
 };
 
@@ -93,62 +115,125 @@ Chunk* Make_Chunk
 
 	float* Target = (float*)malloc(X_Res * Y_Res * Z_Res * 6 * 2 * 3 * 7 * sizeof(float));
 
+	float* Water_Target = (float*)malloc(X_Res * Y_Res * Z_Res * 6 * 2 * 3 * 7 * sizeof(float));
+
 	// Generate the mesh that represents the given subset.
 
-	unsigned int Target_Size_In_Floats = World_Subset_To_Mesh(Input, X, Y, Z, X_Res, Y_Res, Z_Res, Target);
+	unsigned int Target_Size_In_Floats;
+
+	unsigned int Water_Target_Size_In_Floats;
+
+	World_Subset_To_Mesh(Input, X, Y, Z, X_Res, Y_Res, Z_Res, Target, Water_Target, Target_Size_In_Floats, Water_Target_Size_In_Floats);
 
 	// Create the chunk.
 
-	Chunk* The_Chunk = new Chunk(Target_Size_In_Floats, X, Y, Z, X_Res, Y_Res, Z_Res);
+	Chunk* The_Chunk = new Chunk(Target_Size_In_Floats, Water_Target_Size_In_Floats, X, Y, Z, X_Res, Y_Res, Z_Res);
 
-	// Use OpenGL to generate a VBO and a VAO.
+	// Target.
 
-	GLuint Target_VBO;
-	GLuint Target_VAO;
+	{
+		// Use OpenGL to generate a VBO and a VAO.
 
-	glGenVertexArrays(1, &Target_VAO);
+		GLuint Target_VBO;
+		GLuint Target_VAO;
 
-	glGenBuffers(1, &Target_VBO);
+		glGenVertexArrays(1, &Target_VAO);
 
-	// Bind the VBO and the VAO to the current state.
+		glGenBuffers(1, &Target_VBO);
 
-	glBindVertexArray(Target_VAO);
+		// Bind the VBO and the VAO to the current state.
 
-	glBindBuffer(GL_ARRAY_BUFFER, Target_VBO);
+		glBindVertexArray(Target_VAO);
 
-	// Upload the target data to the GPU.
+		glBindBuffer(GL_ARRAY_BUFFER, Target_VBO);
 
-	glBufferData(GL_ARRAY_BUFFER, Target_Size_In_Floats * sizeof(float), Target, GL_STATIC_DRAW);
+		// Upload the target data to the GPU.
 
-	// Free the data from the CPU, as it's on the GPU now.
+		glBufferData(GL_ARRAY_BUFFER, Target_Size_In_Floats * sizeof(float), Target, GL_STATIC_DRAW);
 
-	free(Target);
+		// Free the data from the CPU, as it's on the GPU now.
 
-	// Enable the default vertex attribute pointers.
+		free(Target);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(0 * sizeof(float)));
+		// Enable the default vertex attribute pointers.
 
-	glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(0 * sizeof(float)));
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(0);
 
-	glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(3 * sizeof(float)));
 
-	glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(6 * sizeof(float)));
+		glEnableVertexAttribArray(1);
 
-	glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(6 * sizeof(float)));
 
-	// Unbind the VBO and the VAO from the current state.
+		glEnableVertexAttribArray(2);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+		// Unbind the VBO and the VAO from the current state.
 
-	glBindVertexArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	// Remember the VAO identifier and return the chunk.
+		glBindVertexArray(0);
 
-	The_Chunk->Vertex_Array = Target_VAO;
+		// Store the VBO and VAO addresses.
 
-	The_Chunk->Buffer_Object = Target_VBO;
+		The_Chunk->Vertex_Array = Target_VAO;
+
+		The_Chunk->Buffer_Object = Target_VBO;
+	}
+
+	// Water target.
+
+	{
+		// Use OpenGL to generate a VBO and a VAO.
+
+		GLuint Target_VBO;
+		GLuint Target_VAO;
+
+		glGenVertexArrays(1, &Target_VAO);
+
+		glGenBuffers(1, &Target_VBO);
+
+		// Bind the VBO and the VAO to the current state.
+
+		glBindVertexArray(Target_VAO);
+
+		glBindBuffer(GL_ARRAY_BUFFER, Target_VBO);
+
+		// Upload the target data to the GPU.
+
+		glBufferData(GL_ARRAY_BUFFER, Water_Target_Size_In_Floats * sizeof(float), Water_Target, GL_STATIC_DRAW);
+
+		// Free the data from the CPU, as it's on the GPU now.
+
+		free(Water_Target);
+
+		// Enable the default vertex attribute pointers.
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(0 * sizeof(float)));
+
+		glEnableVertexAttribArray(0);
+
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(3 * sizeof(float)));
+
+		glEnableVertexAttribArray(1);
+
+		glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(6 * sizeof(float)));
+
+		glEnableVertexAttribArray(2);
+
+		// Unbind the VBO and the VAO from the current state.
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		glBindVertexArray(0);
+
+		// Store the VBO and VAO addresses.
+
+		The_Chunk->Water_Vertex_Array = Target_VAO;
+
+		The_Chunk->Water_Buffer_Object = Target_VBO;
+	}
 
 	return The_Chunk;
 }
@@ -166,6 +251,26 @@ inline void Draw_Chunk(Chunk* The_Chunk)
 		// Draw the vertex array object as an array of triangles.
 
 		glDrawArrays(GL_TRIANGLES, 0, The_Chunk->Vertex_Array_Size_In_Floats / 7);
+
+		// Unbind the vertex array object from the current state.
+
+		glBindVertexArray(0);
+	}
+}
+
+// Draw a chunk's water layer.
+
+inline void Draw_Chunk_Water(Chunk* The_Chunk)
+{
+	if (The_Chunk->Water_Vertex_Array_Size_In_Floats > 0 && The_Chunk->Live)
+	{
+		// Bind the vertex array object to the current state.
+
+		glBindVertexArray(The_Chunk->Water_Vertex_Array);
+
+		// Draw the vertex array object as an array of triangles.
+
+		glDrawArrays(GL_TRIANGLES, 0, The_Chunk->Water_Vertex_Array_Size_In_Floats / 7);
 
 		// Unbind the vertex array object from the current state.
 
