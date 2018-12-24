36dae80128c090f72c5348ccdaadefecdb47aea1
CobaltXII <sidatt64@gmail.com>
2018-12-24 16:43:18 -0500

    Added a font fragment shader

---
+++ Shaders/Font_Fragment.glsl
@@ -0,0 +1,59 @@
+#version 330 core
+
+// Frame buffer texture.
+
+uniform sampler2D Screen_Texture;
+
+// Input from vertex shader.
+
+in vec2 Texture_Coordinates;
+
+in float Font_Color;
+
+// Output to OpenGL.
+
+out vec4 Fragment_Color;
+
+// Font colors.
+
+vec4 Font_Colors[16] = vec4[16]
+(
+	vec4(0.0f / 255.0f, 0.0f / 255.0f, 0.0f / 255.0f, 1.0f),
+
+	vec4(0.0f / 255.0f, 0.0f / 255.0f, 170.0f / 255.0f, 1.0f),
+
+	vec4(0.0f / 255.0f, 170.0f / 255.0f, 0.0f / 255.0f, 1.0f),
+
+	vec4(0.0f / 255.0f, 170.0f / 255.0f, 170.0f / 255.0f, 1.0f),
+
+	vec4(170.0f / 255.0f, 0.0f / 255.0f, 0.0f / 255.0f, 1.0f),
+
+	vec4(170.0f / 255.0f, 0.0f / 255.0f, 170.0f / 255.0f, 1.0f),
+
+	vec4(255.0f / 255.0f, 170.0f / 255.0f, 0.0f / 255.0f, 1.0f),
+
+	vec4(170.0f / 255.0f, 170.0f / 255.0f, 170.0f / 255.0f, 1.0f),
+
+	vec4(85.0f / 255.0f, 85.0f / 255.0f, 85.0f / 255.0f, 1.0f),
+
+	vec4(85.0f / 255.0f, 85.0f / 255.0f, 255.0f / 255.0f, 1.0f),
+
+	vec4(85.0f / 255.0f, 255.0f / 255.0f, 85.0f / 255.0f, 1.0f),
+
+	vec4(85.0f / 255.0f, 255.0f / 255.0f, 255.0f / 255.0f, 1.0f),
+
+	vec4(255.0f / 255.0f, 85.0f / 255.0f, 85.0f / 255.0f, 1.0f),
+
+	vec4(255.0f / 255.0f, 85.0f / 255.0f, 255.0f / 255.0f, 1.0f),
+
+	vec4(255.0f / 255.0f, 255.0f / 255.0f, 85.0f / 255.0f, 1.0f),
+
+	vec4(255.0f / 255.0f, 255.0f / 255.0f, 255.0f / 255.0f, 1.0f)
+);
+
+// Main shader code.
+
+void main()
+{
+    Fragment_Color = texture(Screen_Texture, Texture_Coordinates) * Font_Colors[int(Font_Color)];
+}
